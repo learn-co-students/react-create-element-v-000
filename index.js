078ba5c2fc643ed59cb291a3b4ebdd45a7bf76c8
github username <github email address>
2017-05-22 12:52:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const title = React.createElement('h1', {}, 'My First React Code');
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
+const container = React.createElement('div', {}, [title, paragraph]);
+const list =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', { className: 'brown' }, 'Chocolate'),
+        React.createElement('li', { className: 'white' }, 'Vanilla'),
+        React.createElement('li', { className: 'yellow' }, 'Banana')
+      ]
+    ));
 
 ReactDOM.render(
-  title,
+  list,
   document.getElementById('main')
 );
