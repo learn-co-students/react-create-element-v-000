aecfb5e1287d08eb6498196d5cae34d5e815dac7
github username <github email address>
2017-06-13 17:37:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const title = React.createElement('h1', {}, 'My First React Code');
 const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
 const container = React.createElement('div', {}, [title, paragraph]);
@@ -10,7 +13,8 @@ const list =
         React.createElement('li', { className: 'white' }, 'Vanilla'),
         React.createElement('li', { className: 'yellow' }, 'Banana')
       ]
-    ));
+    )
+  );
 
 ReactDOM.render(
   list,
