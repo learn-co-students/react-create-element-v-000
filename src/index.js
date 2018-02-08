f226e3b68f9db1433b27fe58cf232250d9abc263
emorgan05 <emorgan05@gmail.com>
2018-02-08 16:38:32 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-// Code goes here
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const title = React.createElement('h1', {}, 'My First React Code');
+const paragraph = React.createElement('p', {}, 'Writing some more HTML code. Very cool stuff!');
+const container = React.createElement('div', {}, [title, paragraph]);
+
+const list =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'My favorite coding languages'),
+    React.createElement('ul', {},
+    [
+      React.createElement('li', { className: 'brown' }, 'Ruby'),
+      React.createElement('li', { className: 'white' }, 'Rails'),
+      React.createElement('li', { className: 'yellow' }, 'Javascript'),
+    ]
+  )
+);
+
+ReactDom.render(
+  list,
+  document.getElementById('global')
+);
