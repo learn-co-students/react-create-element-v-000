e66f81566f7125d0d166856a9b892cb4e53a50fb
Christian Egglin <cegglin@conncoll.edu>
2018-03-30 16:29:38 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const list = React.createElement('div', {},
   React.createElement('h1', {}, 'My favorite ice cream flavors'),
   React.createElement('ul', {},
     [
-      React.createElement('li', {class: 'brown'}, 'Chocolate'),
-      React.createElement('li', {class: 'white'}, 'Vanilla'),
-      React.createElement('li', {class: 'pink'}, 'Strawberry')
+      React.createElement('li', {className: 'brown'}, 'Chocolate'),
+      React.createElement('li', {className: 'white'}, 'Vanilla'),
+      React.createElement('li', {className: 'pink'}, 'Strawberry')
     ]
   )
 );
